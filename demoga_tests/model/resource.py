7189de3c5024166927a7f3e5c3cb61e56f8ca6ab
Elena Tinkalyuk <lenalenalena3@mail.ru>
2025-06-23 15:52:35 +0300

    корректировка получения пути для файла

--- demoga_tests/model/resource.py
+++ demoga_tests/model/resource.py
@@ -3,5 +3,5 @@ from pathlib import Path
 
 def path(file_name):
     return str(
-        Path(f"./tests/attachment/{file_name}").resolve()
+        (Path(__file__).parents[2] / "tests" / "attachment" / file_name).resolve()
     )
